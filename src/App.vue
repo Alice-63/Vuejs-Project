8f87b5035505629df1f5b1dd0131c433025ffe8d
Alice-63 <alicetindag84@gmail.com>
2021-10-01 11:44:37 +0200

    add

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div>
+<Header/>
+<ProductSell/>
+<Footer/>
+  </div>
 </template>
 <script>
+import Header from './components/shared/Header.vue'
+import Footer from './components/shared/Footer.vue'
+import ProductPurchase from './components/products/ProductPurchase.vue'
+import ProductSell from './components/products/ProductSell.vue'
+
+
+
 export default {
-  
+  components:
+  {
+    Header,
+    Footer,
+    ProductPurchase,
+    ProductSell
+  }
 }
 </script>
 <style scoped>
