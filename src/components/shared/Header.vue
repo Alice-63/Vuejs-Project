9f7934f3500d1bb953a1557a05456b02a8402ebb
Alice-63 <alicetindag84@gmail.com>
2021-10-02 14:32:20 +0200

    Update Header.vue

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563F7A">
-    <a class="navbar-brand" href="#">Ürün Listesi</a>
+    <router-link to="/" class="navbar-brand" >Product List</router-link>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#">Ürün İşlemleri</a>
+                <router-link to="/product-sell" class="nav-link" href="#">Product Sell</router-link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Ürün Çıkışı</a>
+                <router-link to="product-purchase" class="nav-link" href="#">Product Purchase</router-link>
             </li>
         </ul>
     </div>
