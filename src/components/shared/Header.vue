70d6affaa82e9de84f6e22939db53145decb9a81
Alice-63 <alicetindag84@gmail.com>
2021-10-02 14:40:07 +0200

    Update Header.vue

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -5,10 +5,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <router-link to="/product-sell" class="nav-link">Product Sell</router-link>
+                <router-link to="/product-sell" class="nav-link" href="#">Product Sell</router-link>
             </li>
             <li class="nav-item">
-                <router-link to="product-purchase" class="nav-link" >Product Purchase</router-link>
+                <router-link to="product-purchase" class="nav-link" href="#">Product Purchase</router-link>
             </li>
         </ul>
     </div>
