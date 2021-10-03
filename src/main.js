d028e75d3f87a460cdcd3b9bd3112b99abacc298
Alice-63 <alicetindag84@gmail.com>
2021-10-03 08:48:54 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {router} from './router';
+import {store} from './store/modules/store'
 
 
 new Vue({
   el: '#app',
   render: h => h(App),
-router
+router,
+store
 })
--- src/store/modules/store.js
+++ src/store/modules/store.js
@@ -1,3 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import product from "./modules/product"
+import product from "./product"
+
+Vue.use(Vuex)
+
+export const store=new Vuex.Store({
+    state:{},
+    modules:{
+        product
+    }
+})
