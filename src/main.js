4a950820fc5bb07ea54b2bf52a7ed137e093537b
Alice-63 <alicetindag84@gmail.com>
2021-10-03 09:32:32 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {router} from './router';
-import {store} from './store/modules/store'
+import {store} from './store/modules/store';
+import VueResource from 'vue-resource';
 
+Vue.use(VueResource)
 
 new Vue({
   el: '#app',
